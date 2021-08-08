79709dc707ae1ddd3767ed5bb3cd86827e0721b3
mmraihan <mm_raihan@live.com>
2021-08-08 23:22:16 +0600

    Linear Search

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -2,11 +2,27 @@
 
 namespace LinearSearch
 {
-    class Program
+    class Search
     {
+        public int linearSearch(int[] A, int n, int key)
+        {
+            int index = 0;
+            while (index<n)
+            {
+                if (A[index] == key)
+                    return index;
+                index = index + 1;
+            }
+            return -1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Search s = new Search();
+            int[] A = { 84, 20, 44, 33, 22 };
+            int found= s.linearSearch(A, 5, 22);
+            Console.WriteLine("Result: " +found);
+            Console.ReadKey();
+
         }
     }
 }
